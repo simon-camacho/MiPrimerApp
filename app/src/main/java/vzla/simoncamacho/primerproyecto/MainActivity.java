dad01a80a77895e76a57525a2cf393b7ac3aefdd
Jesus Camacho <Jesus Camacho>
2016-09-14 13:47:21 -0400

    Initial commit

---
+++ app/src/main/java/vzla/simoncamacho/primerproyecto/MainActivity.java
@@ -0,0 +1,16 @@
+package vzla.simoncamacho.primerproyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// UNAM
+// Desarrollo de aplicaciones móviles con Android
+// Simon Camacho
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
